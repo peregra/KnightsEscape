1ef9e637cefe9b05d660284433a73b9c4dcf0a23
Brittany Thibodeaux <peregra@gmail.com>
2020-04-24 22:45:15 -0500

    Update KnightsEscape.Build.cs
    Added ApplicationCore

--- Source/KnightsEscape/KnightsEscape.Build.cs
+++ Source/KnightsEscape/KnightsEscape.Build.cs
@@ -8,7 +8,7 @@ public class KnightsEscape : ModuleRules
 	{
         PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "UMG", "AIModule" });
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "UMG", "AIModule", "ApplicationCore" });
 
         PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 
